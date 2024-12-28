4e6bf26227c7ee0220be76698c61e2be99564a16
devgustauz <gustavo_vieira_uz@hotmail.com>
2024-12-28 01:38:34 -0300

    Update CanvasView.java

--- app/src/main/java/com/example/myapp/CanvasView.java
+++ app/src/main/java/com/example/myapp/CanvasView.java
@@ -1,33 +1,22 @@
 package com.example.myapp;
 
- import android.content.Context;
- import android.graphics.Canvas;
- import android.graphics.Color;
- import android.graphics.Paint;
- import android.view.View;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.View;
 
 public class CanvasView extends View {
-
-     private Paint paint;
-     private float xPosition = 0; //Círculo inicia na Esquerda
+    private Paint paint;
 
     public CanvasView(Context context) {
-     super(context);
-     paint = new Paint();
-     paint.setColor(Color.BLUE); //Cor do Círculo
-  }
-  @Override
-  protected void onDraw(Canvas canvas) {
-      super.onDraw(canvas);
- //Desenha um círculo na posição x, 200 com raio de 50
-      canvas.drawCircle(xPosition, 200, 50, paint);
+        super(context);
+        paint = new Paint();
+        paint.setColor(0xFF00FF00);
+    }
 
- //Atualiza a posição do círculo
-      xPosition += 5;
-      if (xPosition > getWidth()) {
-           xPosition = 0; //Reseta ao sair da tela
-      }
-
-      invalidate(); //Redesenha a tela a cada frame
- }
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawCircle(200, 200, 50, paint);
+    }
 }
