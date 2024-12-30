04f416e2a78b147f6427e5876e902a049ad0e7a7
devgustauz <gustavo_vieira_uz@hotmail.com>
2024-12-30 00:27:27 -0300

    Nova Activity

--- app/src/main/java/com/survivalofjack/myapp/MainActivity.java
+++ app/src/main/java/com/survivalofjack/myapp/MainActivity.java
@@ -2,13 +2,30 @@ package com.survivalofjack.myapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import andorid.widget.Butoon;
+
+import com.example.myapplication.R;
 
 public class MainActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // Layout inicial
 
-        CanvasView canvasView = new CanvasView(this);
-        setContentView(canvasView);
-    }
+       // Referência ao botão
+       Button btnPlay = findViewById(R.id.btnPlay);
+
+       // Clique no Botão
+       btnPLay.set.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+
+       Intent intent = new Intent(MainActivity.this, GameActivity.class);
+        startActivity(intent);
+  }
+});
+  }
 }
