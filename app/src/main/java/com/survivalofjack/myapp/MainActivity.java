5b046792ea769e3aa9d23d731c6e845b5e524922
devgustauz <gustavo_vieira_uz@hotmail.com>
2024-12-30 12:11:16 -0300

    Erro de Sintaxe

--- app/src/main/java/com/survivalofjack/myapp/MainActivity.java
+++ app/src/main/java/com/survivalofjack/myapp/MainActivity.java
@@ -2,14 +2,11 @@ package com.survivalofjack.myapp;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
+import android.widget.Toast;
 import android.view.View;
 import android.widget.Button;
 
-import com.survivalofjack.myapp;
-
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,9 +17,7 @@ public class MainActivity extends Activity {
        btnPLay.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View v) {
-
-       Intent intent = new Intent(MainActivity.this, GameActivity.class);
-        startActivity(intent);
+            Toast.makeText(MainActivity.this, "Botão Play Clicado", Toast.LENGTH_SHORT).show();
   }
 });
   }
