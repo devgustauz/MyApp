9f73095896b874b56a1a7710ddd756b39684ff87
devgustauz <gustavo_vieira_uz@hotmail.com>
2024-12-30 23:15:38 -0300

    fullscreen

--- app/src/main/java/com/survivalofjack/myapp/MainActivity.java
+++ app/src/main/java/com/survivalofjack/myapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.widget.Toast;
 import android.view.View;
 import android.widget.Button;
+import android.view.Window;
 import android.view.WindowManager;
 
 public class MainActivity extends Activity {
@@ -13,6 +14,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 
        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
 getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main); // Layout inicial
